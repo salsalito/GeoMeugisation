2413b88bb9b379e75b638083b4a936ccc0d8876c
Meuge <salsalito@gmail.com>
2012-04-12 12:29:57 +0200

    Images

--- src/com/meuge/adapter/InteractiveArrayAdapter.java
+++ src/com/meuge/adapter/InteractiveArrayAdapter.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class InteractiveArrayAdapter extends ArrayAdapter<Model> {
@@ -25,10 +26,25 @@ public class InteractiveArrayAdapter extends ArrayAdapter<Model> {
 	}
 
 	static class ViewHolder {
+		protected ImageView image;
 		protected TextView text;
 		protected CheckBox checkbox;
 	}
 
+	private void iconeLoader(String categ, ImageView tv) {
+		// Depending upon the child type, set the imageTextView01
+        if ("But".equalsIgnoreCase(categ)) 
+        	tv.setImageResource(R.drawable.but);
+        if ("Conforama".equalsIgnoreCase(categ))
+        	tv.setImageResource(R.drawable.conforama);
+        if (categ.startsWith("Darty"))
+        	tv.setImageResource(R.drawable.darty);
+        if ("Boulanger".equalsIgnoreCase(categ))
+        	tv.setImageResource(R.drawable.boulanger);
+        if ("Metro".equalsIgnoreCase(categ))
+        	tv.setImageResource(R.drawable.metro);
+	}
+	
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View view = null;
@@ -36,6 +52,7 @@ public class InteractiveArrayAdapter extends ArrayAdapter<Model> {
 			LayoutInflater inflator = context.getLayoutInflater();
 			view = inflator.inflate(R.layout.parents, null);
 			final ViewHolder viewHolder = new ViewHolder();
+			viewHolder.image = (ImageView) view.findViewById(R.id.logo);
 			viewHolder.text = (TextView) view.findViewById(R.id.grp_parent);
 			viewHolder.checkbox = (CheckBox) view.findViewById(R.id.check);
 			viewHolder.checkbox
@@ -59,6 +76,7 @@ public class InteractiveArrayAdapter extends ArrayAdapter<Model> {
 		ViewHolder holder = (ViewHolder) view.getTag();
 		holder.text.setText(list.get(position).getName());
 		holder.checkbox.setChecked(list.get(position).isSelected());
+		iconeLoader(list.get(position).getName(), holder.image);
 		return view;
 	}
 }
